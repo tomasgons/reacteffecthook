703e41ce0afff82ce5d38c810c4153e0322be031
gonzocode <tomasgons@gmail.com>
2021-11-22 21:59:36 +0100

    3

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -12,9 +12,17 @@ const Login = (props) => {
   const [formIsValid, setFormIsValid] = useState(false);
 
   useEffect(() => {
-    setFormIsValid(
-      enteredEmail.includes('@') && enteredPassword.trim().length > 6
-    );
+
+    const identifier = setTimeout(() => {
+      setFormIsValid(
+        enteredEmail.includes('@') && enteredPassword.trim().length > 6
+      );
+
+    }, 500);
+
+    return () => {
+      clearTimeout(identifier)
+    };
 
   }, [enteredEmail, enteredPassword])
 
